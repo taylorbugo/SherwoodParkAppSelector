87efabb58a8a76fea7ab1ad83eda032cf238e582
taylorbugo <tbugo@kenene.com>
2016-03-08 12:26:57 -0700

    Initial Commit of source from hackathon.

---
+++ SherwoodParkAppSelector/SherwoodParkAppSelector/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace SherwoodParkAppSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Update = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Results = new System.Windows.Forms.TextBox();
+            this.GolfCheck = new System.Windows.Forms.CheckBox();
+            this.BaseballCheck = new System.Windows.Forms.CheckBox();
+            this.CampgroundCheck = new System.Windows.Forms.CheckBox();
+            this.RugbyCheck = new System.Windows.Forms.CheckBox();
+            this.FootballCheck = new System.Windows.Forms.CheckBox();
+            this.TennisCheck = new System.Windows.Forms.CheckBox();
+            this.PlaygroundCheck = new System.Windows.Forms.CheckBox();
+            this.BMX_SkateCheck = new System.Windows.Forms.CheckBox();
+            this.DayUseCheck = new System.Windows.Forms.CheckBox();
+            this.SoccerCheck = new System.Windows.Forms.CheckBox();
+            this.VolleyballCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(163, 476);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(176, 34);
+            this.Update.TabIndex = 0;
+            this.Update.Text = "Update Results";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(742, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Results";
+            // 
+            // Results
+            // 
+            this.Results.Location = new System.Drawing.Point(521, 157);
+            this.Results.Multiline = true;
+            this.Results.Name = "Results";
+            this.Results.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Results.Size = new System.Drawing.Size(499, 279);
+            this.Results.TabIndex = 4;
+            // 
+            // GolfCheck
+            // 
+            this.GolfCheck.AutoSize = true;
+            this.GolfCheck.Location = new System.Drawing.Point(104, 402);
+            this.GolfCheck.Name = "GolfCheck";
+            this.GolfCheck.Size = new System.Drawing.Size(56, 21);
+            this.GolfCheck.TabIndex = 5;
+            this.GolfCheck.Text = "Golf";
+            this.GolfCheck.UseVisualStyleBackColor = true;
+            // 
+            // BaseballCheck
+            // 
+            this.BaseballCheck.AutoSize = true;
+            this.BaseballCheck.Location = new System.Drawing.Point(104, 172);
+            this.BaseballCheck.Name = "BaseballCheck";
+            this.BaseballCheck.Size = new System.Drawing.Size(84, 21);
+            this.BaseballCheck.TabIndex = 6;
+            this.BaseballCheck.Text = "Baseball";
+            this.BaseballCheck.UseVisualStyleBackColor = true;
+            // 
+            // CampgroundCheck
+            // 
+            this.CampgroundCheck.AutoSize = true;
+            this.CampgroundCheck.Location = new System.Drawing.Point(104, 260);
+            this.CampgroundCheck.Name = "CampgroundCheck";
+            this.CampgroundCheck.Size = new System.Drawing.Size(111, 21);
+            this.CampgroundCheck.TabIndex = 7;
+            this.CampgroundCheck.Text = "Campground";
+            this.CampgroundCheck.UseVisualStyleBackColor = true;
+            // 
+            // RugbyCheck
+            // 
+            this.RugbyCheck.AutoSize = true;
+            this.RugbyCheck.Location = new System.Drawing.Point(295, 213);
+            this.RugbyCheck.Name = "RugbyCheck";
+            this.RugbyCheck.Size = new System.Drawing.Size(71, 21);
+            this.RugbyCheck.TabIndex = 8;
+            this.RugbyCheck.Text = "Rugby";
+            this.RugbyCheck.UseVisualStyleBackColor = true;
+            // 
+            // FootballCheck
+            // 
+            this.FootballCheck.AutoSize = true;
+            this.FootballCheck.Location = new System.Drawing.Point(104, 356);
+            this.FootballCheck.Name = "FootballCheck";
+            this.FootballCheck.Size = new System.Drawing.Size(80, 21);
+            this.FootballCheck.TabIndex = 9;
+            this.FootballCheck.Text = "Football";
+            this.FootballCheck.UseVisualStyleBackColor = true;
+            // 
+            // TennisCheck
+            // 
+            this.TennisCheck.AutoSize = true;
+            this.TennisCheck.Location = new System.Drawing.Point(293, 309);
+            this.TennisCheck.Name = "TennisCheck";
+            this.TennisCheck.Size = new System.Drawing.Size(73, 21);
+            this.TennisCheck.TabIndex = 10;
+            this.TennisCheck.Text = "Tennis";
+            this.TennisCheck.UseVisualStyleBackColor = true;
+            // 
+            // PlaygroundCheck
+            // 
+            this.PlaygroundCheck.AutoSize = true;
+            this.PlaygroundCheck.Location = new System.Drawing.Point(293, 172);
+            this.PlaygroundCheck.Name = "PlaygroundCheck";
+            this.PlaygroundCheck.Size = new System.Drawing.Size(102, 21);
+            this.PlaygroundCheck.TabIndex = 11;
+            this.PlaygroundCheck.Text = "Playground";
+            this.PlaygroundCheck.UseVisualStyleBackColor = true;
+            // 
+            // BMX_SkateCheck
+            // 
+            this.BMX_SkateCheck.AutoSize = true;
+            this.BMX_SkateCheck.Location = new System.Drawing.Point(104, 213);
+            this.BMX_SkateCheck.Name = "BMX_SkateCheck";
+            this.BMX_SkateCheck.Size = new System.Drawing.Size(132, 21);
+            this.BMX_SkateCheck.TabIndex = 12;
+            this.BMX_SkateCheck.Text = "BMX/Skate Park";
+            this.BMX_SkateCheck.UseVisualStyleBackColor = true;
+            // 
+            // DayUseCheck
+            // 
+            this.DayUseCheck.AutoSize = true;
+            this.DayUseCheck.Location = new System.Drawing.Point(104, 309);
+            this.DayUseCheck.Name = "DayUseCheck";
+            this.DayUseCheck.Size = new System.Drawing.Size(84, 21);
+            this.DayUseCheck.TabIndex = 13;
+            this.DayUseCheck.Text = "Day Use";
+            this.DayUseCheck.UseVisualStyleBackColor = true;
+            // 
+            // SoccerCheck
+            // 
+            this.SoccerCheck.AutoSize = true;
+            this.SoccerCheck.Location = new System.Drawing.Point(294, 260);
+            this.SoccerCheck.Name = "SoccerCheck";
+            this.SoccerCheck.Size = new System.Drawing.Size(74, 21);
+            this.SoccerCheck.TabIndex = 14;
+            this.SoccerCheck.Text = "Soccer";
+            this.SoccerCheck.UseVisualStyleBackColor = true;
+            // 
+            // VolleyballCheck
+            // 
+            this.VolleyballCheck.AutoSize = true;
+            this.VolleyballCheck.Location = new System.Drawing.Point(294, 356);
+            this.VolleyballCheck.Name = "VolleyballCheck";
+            this.VolleyballCheck.Size = new System.Drawing.Size(90, 21);
+            this.VolleyballCheck.TabIndex = 15;
+            this.VolleyballCheck.Text = "Volleyball";
+            this.VolleyballCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 573);
+            this.Controls.Add(this.VolleyballCheck);
+            this.Controls.Add(this.SoccerCheck);
+            this.Controls.Add(this.DayUseCheck);
+            this.Controls.Add(this.BMX_SkateCheck);
+            this.Controls.Add(this.PlaygroundCheck);
+            this.Controls.Add(this.TennisCheck);
+            this.Controls.Add(this.FootballCheck);
+            this.Controls.Add(this.RugbyCheck);
+            this.Controls.Add(this.CampgroundCheck);
+            this.Controls.Add(this.BaseballCheck);
+            this.Controls.Add(this.GolfCheck);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Update);
+            this.Name = "Form1";
+            this.Text = "Sherwood Park Park Selector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Results;
+        private System.Windows.Forms.CheckBox GolfCheck;
+        private System.Windows.Forms.CheckBox BaseballCheck;
+        private System.Windows.Forms.CheckBox CampgroundCheck;
+        private System.Windows.Forms.CheckBox RugbyCheck;
+        private System.Windows.Forms.CheckBox FootballCheck;
+        private System.Windows.Forms.CheckBox TennisCheck;
+        private System.Windows.Forms.CheckBox PlaygroundCheck;
+        private System.Windows.Forms.CheckBox BMX_SkateCheck;
+        private System.Windows.Forms.CheckBox DayUseCheck;
+        private System.Windows.Forms.CheckBox SoccerCheck;
+        private System.Windows.Forms.CheckBox VolleyballCheck;
+    }
+}
+
---
+++ SherwoodParkAppSelector/SherwoodParkAppSelector/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SherwoodParkAppSelector
+{
+
+    public partial class Form1 : Form
+    {
+        /*
+        //Code for using SQL, was used during the 2016 Open Data Day Hackathon for speed, but not deployable as is
+        private SqlConnection DBConnection;
+        private SqlDataReader DBDataReader;
+        private SqlCommand DBRead;
+        */
+        private Park[] ParkList;
+        private String[] ParsedData;
+        private String Data;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            /*
+            //Code for using SQL, was used during the 2016 Open Data Day Hackathon for speed, but not deployable as is
+            DBConnection = new SqlConnection("Server=KONAMIWA\\SQLEXPRESS;Database=SherwoodParkParks;Trusted_Connection=Yes");
+            DBConnection.Open();
+            DBRead = new SqlCommand("SELECT Park_Name, Golf, Baseball, Campground, Rugby, Football, Tennis, Playground FROM dbo.[Parks Massaged No Spatial]", DBConnection);
+            DBDataReader = DBRead.ExecuteReader();
+            ParkList = new Park[183];
+
+            for (int row = 0; row < 183; row++)
+            {
+                DBDataReader.Read();
+                ParkList[row] = new Park(DBDataReader.GetString(0), DBDataReader.GetString(1), DBDataReader.GetString(2), DBDataReader.GetString(3), DBDataReader.GetString(4), DBDataReader.GetString(5), DBDataReader.GetString(6), DBDataReader.GetString(7));
+            }
+            */
+
+            int offset = 0;
+            
+            Data = System.IO.File.ReadAllText(".\\Parks Massaged No Spatial.csv");
+            Data = Data.Replace("\r\n", ",");
+            ParsedData = Data.Split(',');
+            ParkList = new Park[(ParsedData.Length/12)-1];
+            
+            for (int row = 0; row < (ParsedData.Length/12)-1; row++)
+            {
+                offset = ((row * 12) + 12);
+                ParkList[row] = new Park(ParsedData[offset], ParsedData[offset + 1], ParsedData[offset + 2], ParsedData[offset + 3], ParsedData[offset + 4], ParsedData[offset + 5], ParsedData[offset + 6], ParsedData[offset + 7], ParsedData[offset + 8], ParsedData[offset + 9], ParsedData[offset + 10], ParsedData[offset + 11]);
+            }
+        }
+
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            //Clear the Results and reset the count and ranks
+            Results.Clear();
+            int count = 0;
+            for (int row = 0; row < ParkList.Length; row++)
+            {
+                ParkList[row].Rank = 0;
+            }
+
+            //Set the goal for success
+            if (BaseballCheck.Checked == true)
+            {
+                count++;
+            }
+            if (BMX_SkateCheck.Checked == true)
+            {
+                count++;
+            }
+            if (CampgroundCheck.Checked == true)
+            {
+                count++;
+            }
+            if (DayUseCheck.Checked == true)
+            {
+                count++;
+            }
+            if (FootballCheck.Checked == true)
+            {
+                count++;
+            }
+            if (GolfCheck.Checked == true)
+            {
+                count++;
+            }
+            if (PlaygroundCheck.Checked == true)
+            {
+                count++;
+            }
+            if (RugbyCheck.Checked == true)
+            {
+                count++;
+            }
+            if (SoccerCheck.Checked == true)
+            {
+                count++;
+            }
+            if (TennisCheck.Checked == true)
+            {
+                count++;
+            }
+            if (VolleyballCheck.Checked == true)
+            {
+                count++;
+            }
+
+
+            //Rank the parks for matches
+            if (count > 0)
+                {
+                for (int row = 0; row < ParkList.Length; row++)
+                {
+                    if ((BaseballCheck.Checked == true) && (ParkList[row].Baseball == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((BMX_SkateCheck.Checked == true) && (ParkList[row].BMX_Skate == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((CampgroundCheck.Checked == true) && (ParkList[row].Campground == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((DayUseCheck.Checked == true) && (ParkList[row].Day_Use == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((FootballCheck.Checked == true) && (ParkList[row].Football == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((GolfCheck.Checked == true) && (ParkList[row].Golf == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((PlaygroundCheck.Checked == true) && (ParkList[row].Playground == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((RugbyCheck.Checked == true) && (ParkList[row].Rugby == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((SoccerCheck.Checked == true) && (ParkList[row].Soccer == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((TennisCheck.Checked == true) && (ParkList[row].Tennis == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                    if ((VolleyballCheck.Checked == true) && (ParkList[row].Volleyball == "YES"))
+                    {
+                        ParkList[row].Rank++;
+                    }
+                }
+
+                //Fill out Results Box
+                for (int row = 0; row < ParkList.Length; row++)
+                {
+                    if (count == ParkList[row].Rank)
+                    {
+                        Results.AppendText(ParkList[row].Name + "\n");
+                    }
+                }
+            }
+        }
+            
+
+        public class Park
+        {
+            public String Name;
+            public String Baseball;
+            public String BMX_Skate;
+            public String Campground;
+            public String Day_Use;
+            public String Football;
+            public String Golf;
+            public String Playground;
+            public String Rugby;
+            public String Soccer;
+            public String Tennis;
+            public String Volleyball;
+            public int Rank;
+
+            public Park(String _Name, String _Baseball, String _BMX_Skate, String _Campground, String _Day_Use, String _Football, String _Golf, String _Playground, String _Rugby, String _Soccer, String _Tennis, String _Volleyball)
+            {
+                Name = _Name;
+                Baseball = _Baseball;
+                BMX_Skate = _BMX_Skate;
+                Campground = _Campground;
+                Day_Use = _Day_Use;
+                Football = _Football;
+                Golf = _Golf;
+                Playground = _Playground;
+                Rugby = _Rugby;
+                Soccer = _Soccer;
+                Tennis = _Tennis;
+                Volleyball = _Volleyball;
+                Rank = 0;
+            }
+        }
+
+    }
+}
